1358b715ed6d560c3fa8ed9b939f4adf5c62c979
OmBayus <omerbayramcavus@gmail.com>
2022-07-02 14:40:45 +0300

    Remix Fund Me

---
+++ Remix Fund Me/FallBackExample.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract FallBackExample{
+
+    uint256 public result;
+
+    receive() external payable {
+        result = 1;
+    }
+
+    fallback() external payable{
+        result = 2;
+    }
+
+}
---
+++ Remix Fund Me/FundMe.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "./PriceConverter.sol";
+
+error NotOwner();
+
+contract FundMe{
+
+    using PriceConverter for uint256;
+
+    uint256 public constant MIN_USD = 5 * 1e18;
+
+    address[] public funders;
+    mapping (address => uint256) public addressToAmountFunded;
+
+    address public immutable owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function fund() public payable{
+        require(msg.value.getConversionRate() >= MIN_USD,"Didn't send enough"); // 1e18 = 1* 10 **18
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function balance() public view returns(uint256){
+        return address(this).balance;
+    }
+
+    function withdraw() public onlyOwner{
+        for(uint256 i = 0;i<funders.length;i++){
+            address funder = funders[i];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+
+        // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess,"Send Failed");
+
+        // call
+        (
+            bool callSuccess,
+            // bytes memory dataReturned
+        )= payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess,"Call Failed");
+    }
+
+    modifier onlyOwner{
+        //require(msg.sender == owner,NotOwner());
+        if(msg.sender != owner){ revert NotOwner(); }
+        _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable{
+        fund();
+    }
+}
